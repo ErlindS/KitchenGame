cdf2c5e5bef66b429bc03337154dca8bcf999e9b
Diu <erlindamprogrammieren@gmail.com>
2023-05-26 14:39:54 +0200

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,43 +6,88 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private Gameinput gameinput;
+    [SerializeField] private LayerMask CountersLayerMask;
 
     private bool isWalking;
 
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
+        HandleMovement();
+        HandleIteractions();
+    }
 
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
 
-        inputVector = inputVector.normalized;
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+
+    private void HandleIteractions() {
+
+        Vector2 inputVector = gameinput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, moveDir, out RaycastHit raycasthit, interactDistance, CountersLayerMask))
+        {
+            if (raycasthit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                //Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement() {
+
+        Vector2 inputVector = gameinput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+                if (canMove)
+                {
+
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                }
+            }
+        }
+
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
 
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp( transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        Debug.Log(inputVector);
-    }
-
-    public bool IsWalking() {
-        return isWalking;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        //Debug.Log(inputVector);
     }
 }
